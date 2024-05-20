d8f96af73a785e774b880b41d20d79cbf13bb3ba
vagiz <ahatovvagiz@gmail.com>
2024-05-20 19:34:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Укажите расширение файла и текст для поиска.");
+            return;
+        }
+
+        string extension = args[0];
+        string searchText = args[1];
+
+        string currentDirectory = Directory.GetCurrentDirectory();
+        SearchFiles(currentDirectory, extension, searchText);
+    }
+
+    static void SearchFiles(string directory, string extension, string searchText)
+    {
+        try
+        {
+            string[] files = Directory.GetFiles(directory, $"*.{extension}", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                if (File.ReadAllText(file).Contains(searchText))
+                {
+                    Console.WriteLine($"Файл \"{file}\" содержит текст \"{searchText}\".");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+    }
+}
